67f8c4d72f2bb0610de0f421e91347ed1329a22a
prakash <prakash@divergentcs.org>
2020-10-31 10:00:57 +0530

    first commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <div>Iam the best home component</div>
+      <button onClick={() => console.log("hello")}>Press me!</button>
+    </div>
+  );
+};
+
+export default Home;
